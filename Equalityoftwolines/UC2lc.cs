40be83799a820f7358f571235a3e9030c91c7ca8
Luv sahu <lksahu404@gmail.com>
2022-06-16 15:01:39 +0530

    The code for Equality of two line

---
+++ Equalityoftwolines/UC2lc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComprison
+{
+    internal class UC2lc
+    {
+        public double Lineone()
+        {
+            Console.WriteLine("Enter the value of x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double result = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public double LineTwo()
+        {
+            Console.WriteLine("Enter the value of x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double result1 = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine(result1);
+            return result1;
+        }
+    }
+}
