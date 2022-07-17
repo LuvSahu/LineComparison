303b1c379f59d63af43de4c9d7ef46126b5b792b
Luv sahu <lksahu404@gmail.com>
2022-07-17 19:19:45 +0530

    [Luv SAHU] This code for Line Comparison

---
+++ LineComparison/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LineComprison
+{
+    class Program
+    {
+        public static void Main (String[] args)
+        {
+            Console.WriteLine("==========Welcome To Line Comparison Computation Program==========");
+            
+            //UC1
+            //LineLength lc = new LineLength();
+            //lc.CalculateLength();
+            
+            //UC2
+            //LineLength1 length = new LineLength1();
+            //length.CalculateLength(); //Call The Methods
+            //length.CheckEquality();
+            //Console.ReadLine();
+
+            //UC3
+            //CompareLines compareLines = new CompareLines();
+            //compareLines.CalculateLength(); //Call The Methods
+            //compareLines.CompareTwoLines();
+            //Console.ReadLine();
+
+            //UC5
+
+            AddMultipleContact addMultipleContact = new AddMultipleContact();
+            addMultipleContact.CalculateLength(); //Call The Methods
+            addMultipleContact.CompareTwoLines();
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
